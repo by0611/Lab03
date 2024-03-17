eb7b416950e472fe334d774454aa24b76aeacc77
by0611 <156165417+by0611@users.noreply.github.com>
2024-03-17 09:47:53 +0800

    Update JPA05.java

--- JPA05.java
+++ JPA05.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class JPA05 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        for(int h = 0;h <= 2;h++){
         int total = 1;//設定total為1，因為是相乘
         System.out.println("Please enter one value: ");//輸出
         int n = scn.nextInt();
@@ -9,12 +10,11 @@ public class JPA05 {
             for(int i=1;i<=n;i++){//建立迴圈
                 total *= i;//計算
             }
-            System.out.printf(n+"!= "+total);//輸出
-            scn.close();
-        }
+            System.out.printf(n+"!= "+total+"\n");//輸出
+            }
         else{//判斷
-            System.out.println("Error, the value is out of the range");//輸出
-            scn.close();
+            System.out.println("Error, the value is out of the range \n");//輸出
+            }
         }
-    }
+        scn.close();}
 }
